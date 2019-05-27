488e338193c7788a3b2d1fd16c82a040b2a40979
Gihun Ham <progh2@gmail.com>
2019-05-27 09:03:33 +0900

    #2 타이머 동작 코드 구현

--- Timer/Form1.Designer.cs
+++ Timer/Form1.Designer.cs
@@ -38,6 +38,7 @@
             // Timer
             // 
             this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
             // 
             // txtNum
             // 
@@ -46,6 +47,7 @@
             this.txtNum.Size = new System.Drawing.Size(191, 21);
             this.txtNum.TabIndex = 0;
             this.txtNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtNum_KeyPress);
             // 
             // btnCount
             // 
@@ -55,6 +57,7 @@
             this.btnCount.TabIndex = 1;
             this.btnCount.Text = "카운트 다운";
             this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.BtnCount_Click);
             // 
             // txtCountDown
             // 
--- Timer/Form1.cs
+++ Timer/Form1.cs
@@ -13,9 +13,73 @@ namespace Timer
 {
     public partial class Form1 : Form
     {
+        int CountOrgNum = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnCount_Click(object sender, EventArgs e)
+        {
+            ProcessTimer();
+        }
+
+        private void ProcessTimer()
+        {
+            if (IntCheck())
+            {
+                this.CountOrgNum = Convert.ToInt32(this.txtNum.Text);
+                this.txtNum.ReadOnly = true;
+                this.Timer.Enabled = true;
+            }
+            else
+            {
+                this.txtNum.Focus();
+                this.txtNum.Text = "";
+            }
+        }
+
+        private bool IntCheck()
+        {
+            if (Information.IsNumeric(this.txtNum.Text))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("숫자만 입력해 주세요", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if(this.CountOrgNum < 1)
+            {
+                this.Timer.Enabled = false;
+                MessageBox.Show("펑!", "알림",
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Information);
+                this.txtNum.ReadOnly = false;
+                this.txtNum.Text = "";
+                this.txtNum.Focus();
+            }
+            else
+            {
+                this.CountOrgNum = this.CountOrgNum - 1;
+                this.txtCountDown.Text = 
+                    Convert.ToString(this.CountOrgNum);
+            }
+        }
+
+        private void TxtNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                ProcessTimer();
+            }
         }
     }
 }
